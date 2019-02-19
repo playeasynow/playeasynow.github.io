a7e80cd589c19865ffaa1d50e8027740354e855e
Play Easy Now <16202860+playeasynow@users.noreply.github.com>
2019-02-18 21:48:21 -0600

    added react, cleaned up

---
+++ portfolio/src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <footer>
+            <hr />
+            <p className="text-center">
+                <i className="fab fa-github" /> built by React.js <br /> powered by Google Books API
+            </p>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ portfolio/src/components/Nav/index.js
@@ -0,0 +1,69 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./style.css";
+
+class Nav extends Component {
+    state = {
+        open: false,
+        width: window.innerWidth
+    };
+
+    updateWidth = () => {
+        const newState = { width: window.innerWidth };
+
+        if (this.state.open && newState.width > 991) {
+            newState.open = false;
+        }
+        this.setState(newState);
+    };
+
+    toggleNav = () => {
+        this.setState({ open: !this.state.open });
+    };
+
+    componentDidMount() {
+        window.addEventListener("resize", this.updateWidth);
+    }
+
+    // eslint-disable-next-line react/no-typos
+    componentWillUnMount() {
+        window.removeEventListener("resize", this.updateWidth);
+    }
+
+    render() {
+        return (
+            <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+                <Link className="navbar-brand text-white" to="/">
+                <h2><i><b>Google Books</b></i></h2>
+                </Link>
+                <button
+                    onClick={this.toggleNav}
+                    className="navbar-toggler"
+                    data-toggle="collapse"
+                    data-target="#navbarNav"
+                    aria-controls="navbarNav"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                    >
+                        <span className="navbar-toggler-icon" />
+                    </button>
+                    <div className={`${this.state.open ? "" : "collapse "}navbar-collapse navBtn`} id="navbarNav">
+                        <ul className="navbar-item list-unstyled">
+                            <li className="nav-item mt-3">
+                                <Link
+                                onClick={this.toggleNav}
+                                className={window.location.pethname === "/" ? "nav-link active" : "nav-link"}
+                                to="/saved"
+                                >
+                                Saved Books
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+            </nav>
+
+        );
+    }
+}
+
+export default Nav;
